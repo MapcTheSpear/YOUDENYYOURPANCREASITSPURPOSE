d0db1dd5fa3f5df2c1626ec4504411665839ad02
MapcTheSpear <damnnewaccin10yrs@gmail.com>
2023-11-21 02:36:08 +0600

    hw 4

--- keyboards/inline_buttons.py
+++ keyboards/inline_buttons.py
@@ -11,8 +11,18 @@ async def start_keyboard():
         "Register here, NOW",
         callback_data="registration"
     )
+    my_profile_button = InlineKeyboardButton(
+        "My profile, nigga",
+        callback_data="my_profile"
+    )
+    random_profiles_button = InlineKeyboardButton(
+        "Show me other niggas",
+        callback_data="random_profiles"
+    )
     markup.add(questionnaire_button)
     markup.add(registration_button)
+    markup.add(my_profile_button)
+    markup.add(random_profiles_button)
     return markup
 
 
@@ -28,4 +38,38 @@ async def questionnaire_keyboard():
     )
     markup.add(bluds_button)
     markup.add(crips_button)
+    return markup
+
+async def like_dislike_keyboard(owner_tg_id):
+    markup = InlineKeyboardMarkup()
+    like_button = InlineKeyboardButton(
+        "Like yo",
+        callback_data=f"liked_profile {owner_tg_id}"
+    )
+    dislike_button = InlineKeyboardButton(
+        "Eww wtf",
+        callback_data="random_profiles"
+    )
+    markup.add(like_button)
+    markup.add(dislike_button)
+    return markup
+
+
+async def my_profile_keyboard():
+    markup = InlineKeyboardMarkup()
+    edit_button = InlineKeyboardButton(
+        "Edit yo",
+        callback_data=f"update_profile"
+    )
+    delete_button = InlineKeyboardButton(
+        "Delete this",
+        callback_data="delete"
+    )
+    back_button = InlineKeyboardButton(
+        "Get back",
+        callback_data="back"
+    )
+    markup.add(edit_button)
+    markup.add(delete_button)
+    markup.add(back_button)
     return markup
